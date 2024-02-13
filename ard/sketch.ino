e088a6396f1cbde9176fbc473427d40b3bc07e9a
Dmitriy <67872855+IMalygosI@users.noreply.github.com>
2024-02-13 16:15:32 +0300

    Add files via upload

---
+++ ard/sketch.ino
@@ -0,0 +1,50 @@
+int ledPin = 4;
+
+
+void setup() {  
+pinMode(ledPin,OUTPUT);
+
+}
+void loop() {
+
+    digitalWrite(ledPin, HIGH);    
+    delay(500);  
+    digitalWrite(ledPin, LOW);    
+    delay(500);  
+    digitalWrite(ledPin, HIGH);    
+    delay(500);   
+    digitalWrite(ledPin, LOW);    
+    delay(500);   
+    digitalWrite(ledPin, HIGH);    
+    delay(500);
+    digitalWrite(ledPin, LOW);    
+    delay(1000);  
+
+    digitalWrite(ledPin, HIGH);    
+    delay(1500); 
+    digitalWrite(ledPin, LOW);    
+    delay(1000);   
+    digitalWrite(ledPin, HIGH);    
+    delay(1500);  
+    digitalWrite(ledPin, LOW);    
+    delay(1000);  
+    digitalWrite(ledPin, HIGH);    
+    delay(1500);
+    digitalWrite(ledPin, LOW);    
+    delay(1000); 
+
+    digitalWrite(ledPin, HIGH);    
+    delay(500);  
+    digitalWrite(ledPin, LOW);    
+    delay(500);    
+    digitalWrite(ledPin, HIGH);    
+    delay(500);   
+    digitalWrite(ledPin, LOW);    
+    delay(500);   
+    digitalWrite(ledPin, HIGH);    
+    delay(500);
+    digitalWrite(ledPin, LOW);    
+    delay(1000);  
+ 
+
+}
