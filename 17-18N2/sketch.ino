e9a41f859a2ab213d138ad65d410dfd258a7c127
Dmitriy <67872855+IMalygosI@users.noreply.github.com>
2024-02-19 15:42:37 +0300

    Add files via upload

---
+++ 17-18N2/sketch.ino
@@ -0,0 +1,35 @@
+#define BUZZER_PIN 3
+#define LDR_PIN    A0
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    int val, frequency;
+    val = analogRead(LDR_PIN);
+    frequency = map(val, 0, 1023, 3500, 4500);
+
+    tone(BUZZER_PIN, frequency, 200);
+    delay(500);
+    tone(BUZZER_PIN, frequency, 200);
+    delay(500);
+    tone(BUZZER_PIN, frequency, 200);
+    delay(500);
+    
+    tone(BUZZER_PIN, frequency, 200);
+    delay(2000);
+    tone(BUZZER_PIN, frequency, 200);
+    delay(2000);
+    tone(BUZZER_PIN, frequency, 200);
+    delay(2000);
+    
+    tone(BUZZER_PIN, frequency, 200);
+    delay(500);
+    tone(BUZZER_PIN, frequency, 200);
+    delay(500);
+    tone(BUZZER_PIN, frequency, 200);
+    delay(500);  
+}
